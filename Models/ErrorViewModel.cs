8e3c94ab1eb9f080566a5a3ef3fdf4ef7e3c43c8
rtxcet <cetinrtx@gmail.com>
2025-09-19 21:27:28 +0300

    Create asp.net core

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace vipkuryen.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
